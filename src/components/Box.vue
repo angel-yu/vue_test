f9614877497a55644f9b21a593950d4c7887a3fd
angel-yu <ge2190162725@163.com>
2021-05-11 16:59:15 +0800

    新增了Box

---
+++ src/components/Box.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.box{
+  width: 200px;
+  height: 200px;
+  background-color: #00b3d4;
+}
+</style>
