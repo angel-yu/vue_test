eb43de59dec319329bd8c271adf39b6d60941822
angel-yu <ge2190162725@163.com>
2021-05-11 17:43:59 +0800

    合并img

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Img />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Img from './components/Img.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Img
   }
 }
 </script>
